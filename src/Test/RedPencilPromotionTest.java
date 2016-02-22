f39bb7b51701cd2e99234d9c61aa7b7738939b52
Kevin <khfire22@gmail.com>
2016-02-21 20:09:17 -0500

    promoEligible fail

---
+++ src/Test/RedPencilPromotionTest.java
@@ -0,0 +1,42 @@
+package Test;
+
+import com.kevinhodges.RedPencilPromotion;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/*
+    These parameters outline the requirements for the Red Pencil Promotion
+
+    1)  Starts only after 30 days of stable price
+    2)  Starts when price is reduced by at least 5% but no more than 30%
+    3)  Lasts up to 30 days max, no minimum time specified
+    4a) If the price is reduced further, the length of the promo does not change
+    4b) Unless the price is reduced by over 30% of original, this will end the promo immediately
+    5)  If the price is increased at any point, the promo will end immediately
+    6)  After the promo ends, others may follow as long as condition (1) is met
+*/
+
+public class RedPencilPromotionTest {
+
+    private RedPencilPromotion promo;
+
+    @Before
+    public void setUp() {
+        promo = new RedPencilPromotion();
+    }
+
+    @Test
+    public void promoEligible() {
+        //Arrange
+
+        //Act
+        boolean result = promo.getPromoEligibility();
+
+        //Assert
+        assertTrue(result);
+    }
+}
+
+
---
+++ src/com/kevinhodges/RedPencilPromotion.java
@@ -0,0 +1,8 @@
+package com.kevinhodges;
+
+public class RedPencilPromotion {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
