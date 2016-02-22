6adf5aa827ac0d85b3133dfdf8c4ab771494c907
Kevin <khfire22@gmail.com>
2016-02-21 20:16:33 -0500

    getPromoEligibility pass

--- src/com/kevinhodges/RedPencilPromotion.java
+++ src/com/kevinhodges/RedPencilPromotion.java
@@ -5,4 +5,13 @@ public class RedPencilPromotion {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    // Getter for promo eligibility
+    // If price has been stable for 30 or more days, return true
+    public boolean getPromoEligibility(int daysOfStablePrice) {
+        if (daysOfStablePrice >= 30) {
+            return true;
+        }
+        return false;
+    }
 }
