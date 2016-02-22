b3840848eaba2d49721c166ab0272c19f32b374b
Kevin <khfire22@gmail.com>
2016-02-22 15:48:50 -0500

    refactor

--- src/Test/RedPencilPromotionTest.java
+++ src/Test/RedPencilPromotionTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/*
+/**
     These parameters outline the requirements for the Red Pencil Promotion
     1)  Starts only after 30 days of stable price
     2)  Starts when price is reduced by at least 5% but no more than 30%
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertTrue;
     4b) Unless the price is reduced by over 30% of original, this will end the promo immediately
     5)  If the price is increased at any point, the promo will end immediately
     6)  After the promo ends, others may follow as long as condition (1) is met
-*/
+**/
 
 public class RedPencilPromotionTest {
 
--- src/com/kevinhodges/RedPencilPromotion.java
+++ src/com/kevinhodges/RedPencilPromotion.java
@@ -10,7 +10,9 @@ public class RedPencilPromotion {
     public static void main(String[] args) {
     }
 
-    // Default constructor
+    /**
+     * Default constructor
+     */
     public RedPencilPromotion() {
     }
 
@@ -30,8 +32,10 @@ public class RedPencilPromotion {
         mNewPrice = newPrice;
     }
 
-
-    // Check to see if there is currently a RPP
+    /**
+     * Check to see if there is currently a RPP
+     * @return true if there is currently a Red Pencil Promotion
+     */
     public boolean isThereARedPencilPromotion() {
 
         return isPriceStableForAtLeast30Days(mDaysOfStablePrice)
@@ -40,9 +44,12 @@ public class RedPencilPromotion {
                 && !isPriceIncreased(mOriginalPrice, mNewPrice);
     }
 
-
-    // Determine if promo is inside the eligible period
-    // If price has been stable for 30 or more days, return true
+    /**
+     * Determine if the promo is eligible with a stable price of at least 30 days
+     *
+     * @param daysOfStablePrice amount of days the price has been stable
+     * @return true if price has been stable for at least 30 days
+     */
     public boolean isPriceStableForAtLeast30Days(int daysOfStablePrice) {
         if (daysOfStablePrice >= 30) {
             return true;
@@ -50,7 +57,14 @@ public class RedPencilPromotion {
         return false;
     }
 
-    // Method to determine if the sale is appropriate for the Red Pencil Promotion
+    /**
+     * Method to determine if the sale is appropriate for the Red Pencil Promotion
+     * sale must be between 5 and 30 percent off regular price
+     *
+     * @param originalPrice the original starting price
+     * @param newPrice the discounted/new price
+     * @return true if discount is within 5 and 30 percent
+     */
     public static boolean isPriceReductionBetween5and30Percent(double originalPrice, double newPrice) {
 
         // Subtract the reduced price from the original price, then divide that by the original price
@@ -64,7 +78,12 @@ public class RedPencilPromotion {
         return false;
     }
 
-    // Method to determine if the promo has extended past the max length of 30 days
+    /**
+     * Method to determin if promo has extended past the max length of 30 days
+     *
+     * @param promoDays the length of the promo in days
+     * @return true if the promo length is less than or equal to 30 days
+     */
     public static boolean isPromoLengthLessThan30Days(int promoDays) {
         if (promoDays <= 30) {
             return true;
@@ -72,7 +91,13 @@ public class RedPencilPromotion {
         return false;
     }
 
-    // Method to determine if promo price increases at any point
+    /**
+     * Method to determine if the price increases at any point
+     *
+     * @param originalPrice original price
+     * @param newPrice new price
+     * @return true if the new price is higher than the original
+     */
     public static boolean isPriceIncreased(double originalPrice, double newPrice) {
 
         if (newPrice > originalPrice) {
